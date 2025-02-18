e2e3cd8c473b5fafa97b04901b5eb62ef855b9aa
PeterNgeno <peterkipsangngeno@gmail.com>
2025-02-18 21:59:59 +0300

    Create userAnalytics.js

---
+++ analytics/userAnalytics.js
@@ -0,0 +1,5 @@
+// Sample logic to collect and report user analytics.
+exports.logUserActivity = (userId, action) => {
+  // Save the activity log to your database or analytics service.
+  console.log(`User ${userId} performed action: ${action}`);
+};
